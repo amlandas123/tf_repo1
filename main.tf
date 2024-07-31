bf4db7a60f533d68323b9eba2072ebd0404e2854
amlandas123 <56760388+amlandas123@users.noreply.github.com>
2024-07-31 12:39:13 +0530

    Update main.tf001234

--- main.tf
+++ main.tf
@@ -1,14 +1,57 @@
 provider "aws" {
-   
+    access_key = var.access_key
+    secret_key = var.secret_key
     region = "us-east-1"
+
 }
 
 
-resource "aws_vpc" "amlandas-vpc" {
-    cidr_block = "192.169.0.0/24"
-    tags = {
-      "Name"= "dasabihan2024"
-    }
-  
+variable "access_key" {
+    type = string 
 }
+
+variable "secret_key" {
+
+    type = string  
+}
+
+
+variable "keyname" {
+
+ type = string
+
+}
+
+
+
+resource "tls_private_key" "rsa" {
+
+algorithm = "RSA"
+
+rsa_bits = 4096
+
+}
+
+
+
+resource "aws_key_pair" "tf-key-pair" {
+
+key_name = var.keyname
+
+public_key = tls_private_key.rsa.public_key_openssh
+
+}
+
+
+
+resource "local_file" "tf-key" {
+
+content = tls_private_key.rsa.private_key_pem
+
+filename = var.keyname
+
+}
+
+
+
 

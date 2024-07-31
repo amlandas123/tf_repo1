6ce3d2430bc30fdc6fd9ab639ec1f96babf37537
amlandas123 <56760388+amlandas123@users.noreply.github.com>
2024-07-31 11:35:17 +0530

    Update main.tf2345

--- main.tf
+++ main.tf
@@ -1,22 +1,13 @@
 provider "aws" {
-    access_key = var.access_key
-    secret_key = var.secret_key
+   
     region = "us-east-1"
 }
 
 
-variable "access_key" {
-    type = string 
-}
-
-variable "secret_key" {
-    type = string  
-}
-
 resource "aws_vpc" "amlandas-vpc" {
     cidr_block = "192.169.0.0/24"
     tags = {
-      "Name"= "dasabihan123"
+      "Name"= "dasabihan2024"
     }
   
 }
